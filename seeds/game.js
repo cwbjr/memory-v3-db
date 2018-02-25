bce0a621e7ba2bb8473c64d2b0b8fba46c8a972b
Charles Ballew <charleswballewjr@gmail.com>
2018-02-24 22:17:06 -0700

    seeded table

--- seeds/game.js
+++ seeds/game.js
@@ -1,13 +1,30 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('game').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('game').insert([
+        {
+          id: 1,
+          name: 'Breath of the Wild',
+          developer: 'Nintendo',
+          rating: 8.5
+        },
+        {
+          id: 2,
+          name: 'Fez',
+          developer: 'Polytron',
+          rating: 7.4
+        },
+        {
+          id: 3,
+          name: 'Anti-Chamber',
+          developer: 'Demruth',
+          rating: 8.1
+        }
       ]);
+    }).then(() => {
+      return knex.raw('ALTER SEQUENCE game_id_seq RESTART WITH 4;');
     });
 };
