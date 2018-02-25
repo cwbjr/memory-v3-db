1e1e907300df637d4fc70a6593e8724a883f9729
Charles Ballew <charleswballewjr@gmail.com>
2018-02-24 22:48:25 -0700

    removed semi-colon app.js line 21

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get('env') === 'development' ? err.stack : {};
+    error: req.app.get('env') === 'development' ? err.stack : {}
   });
 });
 
